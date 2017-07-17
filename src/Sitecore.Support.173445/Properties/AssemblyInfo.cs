a1363f64303d1a60c6299ee1b5ee3e26684bea71
Dmitry Kolinchuk <dmko@sitecore.net>
2017-07-17 14:09:53 +0300

    Replace <p></p> with <p>&nbsp;</p> after saving the raw value of the Rich Text Editor field

---
+++ src/Sitecore.Support.173445/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.173445")]
+[assembly: AssemblyProduct("Sitecore.Support.173445")]
+[assembly: ComVisible(false)]
